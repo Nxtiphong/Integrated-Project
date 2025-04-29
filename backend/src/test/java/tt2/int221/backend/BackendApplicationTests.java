df78b2d1c78be97df1ce63db197910ea9edcac2b
Jettaphum Chantaweekhun <jettaphoom@gmail.com>
2025-04-29 14:47:33 +0700

    ✨ Project Initialize

---
+++ backend/src/test/java/tt2/int221/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tt2.int221.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ frontend/src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-2xl text-green-500 font-bold">Hello World</h1>
+</template>
+
+<style scoped></style>

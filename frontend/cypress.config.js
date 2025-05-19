287f29756d69d618bf7cf290441e95664b748577
Nxtiphong <naynaynay2233@gmail.com>
2025-05-19 14:02:25 +0700

    cypress

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://ip24tt2.sit.kmutt.ac.th',
+    baseUrl: 'http://intproj24.sit.kmutt.ac.th/tt2/',
     baseAPI: 'http://ip24tt2.sit.kmutt.ac.th:8080/itb-mshop',
     setupNodeEvents(on, config) {
       // implement node event listeners here
